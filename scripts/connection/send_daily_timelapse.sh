e20c64a3df4954e1a08bd304b12f6f03894a22ec
Naikipy <naikibro@gmail.com>
2024-03-26 15:18:08 -1000

    feat: finalized core scripts
    - take_photo.py OK
    - compile_day.py
    - send_daily_timelapse.sh OK
    - flush_daily_data.sh OK

---
+++ scripts/connection/send_daily_timelapse.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Load environment variables from .env file
+if [[ -f .env ]]; then
+    source .env
+else
+    echo "Environment file .env not found."
+    exit 1
+fi
+
+# Check if destination folder exists on the remote server, if not, create it
+ssh "$USER@$VIDEO_SERVER_URL" "mkdir -p $VIDEOS_FOLDER"
+
+# Upload the contents of the local VIDEOS_FOLDER directory to the remote VIDEOS_FOLDER directory
+scp -r "$VIDEOS_FOLDER/" "$USER@$VIDEO_SERVER_URL:$VIDEOS_FOLDER/../"
--- scripts/flush_daily_data.sh
+++ scripts/flush_daily_data.sh
@@ -4,15 +4,20 @@
 if [[ -f .env ]]; then
     source .env
 else
-    echo "Environment file .env not found."
+    echo "Error: Environment file .env not found."
     exit 1
 fi
 
 # Function to calculate total file size in a folder
 calculate_total_size() {
     local folder="$1"
-    local total_size=$(du -sb "$folder" | cut -f1) # Summarize total size of files in bytes
-    echo "$total_size"
+    if [[ -d "$folder" ]]; then
+        local total_size=$(du -sb "$folder" | cut -f1) # Summarize total size of files in bytes
+        echo "$total_size"
+    else
+        echo "Error: Folder $folder does not exist."
+        exit 1
+    fi
 }
 
 # Function to delete files and log total size
@@ -24,17 +29,20 @@ delete_and_log() {
     # Calculate the total size before deleting
     total_size=$(calculate_total_size "$folder")
 
-    # Delete the files in the folder
-    rm -rf "$folder"/*
-
-    # Get timestamp
-    timestamp=$(date +"%Y-%m-%d %H:%M:%S")
-
-    # Log the total size released with timestamp
-    echo "$timestamp - Total size released: $total_size bytes" >> "$log_file"
+    # Delete the files in the folder if it exists
+    if [[ -d "$folder" ]]; then
+        rm -rf "$folder"/*
+        # Get timestamp
+        timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+        # Log the total size released with timestamp
+        echo "$timestamp - Total size released: $total_size bytes" >> "$log_file"
+    else
+        echo "Error: Folder $folder does not exist."
+        exit 1
+    fi
 }
 
-# Delete videos, images and log total size
+# Delete videos, images, and log total size
 delete_and_log "$VIDEOS_FOLDER" "$SIZE_HANDLING_LOG"
 delete_and_log "$IMAGES_FOLDER" "$SIZE_HANDLING_LOG"
 
