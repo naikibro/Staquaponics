c412d99493836b6d6d1aa0e88507d8f397cb0df9
Naikipy <naikibro@gmail.com>
2024-04-02 20:07:03 -1000

    fix: handle env vars in cron jobs
    - cron jobs dont have env var context
    - added context in scripts
    - added context in user crontab
    - added context in setup script

---
+++ scripts/sync.sh
@@ -0,0 +1,3 @@
+/usr/bin/python3 /home/naiki/Desktop/Staquaponics/scripts/compile_day.py
+/bin/bash /home/naiki/Desktop/Staquaponics/scripts/connection/send_daily_timelapse.sh
+/bin/bash /home/naiki/Desktop/Staquaponics/scripts/flush_daily_data.sh
